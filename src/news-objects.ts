47c0ee9048d29b020d6be5fe24056c7df0dafa00
Tyler Ayers <tylerayers@carbon7.localdomain>
2021-12-28 10:18:11 +0100

    first version

---
+++ src/news-objects.ts
@@ -0,0 +1,37 @@
+export interface News {
+  locations: Record<string, Location>;
+  articles: Article[];
+}
+
+interface Location {
+  country: string;
+  code: string;
+  geometry: {
+    location: Geo;
+    viewport: {
+      northeast: Geo;
+      southwest: Geo;
+    }
+  }
+  city: string;
+  flag?: string;
+}
+
+interface Geo {
+  lat: string;
+  lng: string;
+}
+
+interface Article {
+  source: string;
+  region: string;
+  language: string;
+  icon: string;
+  id: string;
+  title: string;
+  description: string;
+  link: string;
+  published: string;
+  tags: string[];
+  location: string;
+}
